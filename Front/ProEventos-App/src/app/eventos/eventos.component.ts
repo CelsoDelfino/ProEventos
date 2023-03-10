6abcf1e142be77eb9f0e6a56c2cc7d51983bd5b2
Celso Delfino <celso.delfino@capgemini.com>
2023-03-10 16:26:26 -0300

    fim sessao5

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-eventos',
@@ -7,17 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit(): void{
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor)!== -1
+    );
+  }
+
+  ngOnInit(): void {
     this.getEventos();
+  }
+
+  alterarImagem() {
+    this.exibirImagem = !this.exibirImagem;
+    console.log(this.exibirImagem);
   }
 
   public getEventos(): any {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
   }
